250877cfcaf09c8e962f80dfb57fcbb8003d86e4
monty <monty@super>
2022-09-29 10:35:43 +0530

    FIRST COMMIT

---
+++ Day1_GitHubConnection/src/com/tnsindia/basic/TestingEclipseGitConnection.java
@@ -0,0 +1,11 @@
+package com.tnsindia.basic;
+
+public class TestingEclipseGitConnection {
+
+	public static void main(String[] args) {
+		// To print statement..Single line comment
+		System.out.println("Welcom to M25_C2TC Batch");
+
+	}
+
+}
