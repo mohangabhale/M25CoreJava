e1635510f929fe442128fa637608cf9ec0a6ea82
monty <monty@super>
2022-10-30 17:07:31 +0530

    commit

---
+++ Day12_Lambda-Expression/src/org/tnsindia/lambdaexpdemo/FunctionalInterfaceDemo.java
@@ -0,0 +1,9 @@
+package org.tnsindia.lambdaexpdemo;
+
+@FunctionalInterface
+public interface FunctionalInterfaceDemo {
+	//1.Functional interface contains exactly one abstract method
+		//abstract method
+		String display(String str);
+
+}
